6f4e75868182a3ba16dc1ac2c46cb9189d7ec797
Code Help Tutor <1823890830@qq.com>
2024-03-12 04:15:21 +0000

    WeChat cstutorcs CSE224

---
+++ CSE224 proj2 TritonHTTP/tritonhttp/constants.go
@@ -0,0 +1,12 @@
+WeChat: cstutorcs
+QQ: 749389476
+Email: tutorcs@163.com
+package tritonhttp
+
+import "time"
+
+const (
+	CONNECT_TIMEOUT time.Duration = 5 * time.Second
+	SEND_TIMEOUT    time.Duration = 5 * time.Second
+	RECV_TIMEOUT    time.Duration = 5 * time.Second
+)
---
+++ CSE224 proj4 Scalable Surfstore/pkg/surfstore/ConsistentHashRing.go
@@ -0,0 +1,28 @@
+WeChat: cstutorcs
+QQ: 749389476
+Email: tutorcs@163.com
+package surfstore
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+)
+
+type ConsistentHashRing struct {
+	ServerMap map[string]string
+}
+
+func (c ConsistentHashRing) GetResponsibleServer(blockId string) string {
+	panic("todo")
+}
+
+func (c ConsistentHashRing) Hash(addr string) string {
+	h := sha256.New()
+	h.Write([]byte(addr))
+	return hex.EncodeToString(h.Sum(nil))
+
+}
+
+func NewConsistentHashRing(serverAddrs []string) *ConsistentHashRing {
+	panic("todo")
+}
